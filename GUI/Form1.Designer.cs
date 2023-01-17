03f6e9323e810fd52f15ea3abf15b8018dfd4771
Jin Terada <jintw@uw.edu>
2023-01-17 04:12:42 -0800

    GUI Coordinate Gen and MMF initial commit. Confirmed to work with mono

---
+++ GUI/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿using System.Linq.Expressions;
+
+namespace GUI {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose (bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent () {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(733, 648);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(344, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(700, 317);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(407, 54);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "A very quick demo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS Gothic", 14.1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(80, 487);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(153, 78);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "???";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1763, 706);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private int offsetCount;
+        private const int WORD_OFFSET = 4;
+        private unsafe byte* generatorMemPtr;
+        private unsafe byte* readerMemPtr;
+        private unsafe byte* beginPtr;
+    }
+}
+
---
+++ GUI/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace GUI {
+    public partial class Form1 : Form {
+        public Form1 () {
+            InitializeComponent();
+            this.offsetCount = 0;
+            InitMMF();
+        }
+
+        private unsafe void InitMMF () {
+            // Allocate a block of unmanaged memory and return an IntPtr object.	
+            IntPtr memIntPtr = Marshal.AllocHGlobal(WORD_OFFSET * 4 * 2);
+
+            // Get a byte pointer from the IntPtr object.
+            this.generatorMemPtr = (byte*) memIntPtr.ToPointer();
+
+            // Get another for the lagger
+            this.readerMemPtr = (byte*) memIntPtr.ToPointer();
+
+            // Save start for freeing mem on exit
+            this.beginPtr = (byte*) memIntPtr.ToPointer();
+        }
+
+        private unsafe void button1_Click (object sender, EventArgs e) {
+            // Free the block of unmanaged memory.
+            Marshal.FreeHGlobal((IntPtr) this.beginPtr);
+            Application.Exit();
+        }
+
+        private void textBox1_TextChanged (object sender, EventArgs e) {
+
+        }
+
+        private void textBox1_TextChanged_1 (object sender, EventArgs e) {
+
+        }
+
+        private void Form1_Load (object sender, EventArgs e) {
+
+        }
+
+        private void Form1_MouseMove (object sender, MouseEventArgs e) {
+            // label1.Text = Cursor.Position.ToString();
+            label1.Text = e.Location.ToString();
+            testWriter(e.Location.X, e.Location.Y);
+        }
+
+        private unsafe void testWriter (int x, int y) {
+            // Create some data to read and write.
+            UInt16 xCoord = (UInt16) x;
+            UInt16 yCoord = (UInt16) y;
+            byte[] message = new byte[4];
+            message[0] = (byte) (xCoord >> 8);
+            message[1] = (byte) xCoord;
+            message[2] = (byte) (yCoord >> 8);
+            message[3] = (byte) yCoord;
+
+
+            //byte[] message = UnicodeEncoding.Unicode.GetBytes(x + " " + y);
+
+            // Create an UnmanagedMemoryStream object using a pointer to unmanaged memory.
+            UnmanagedMemoryStream writeStream = new UnmanagedMemoryStream(this.generatorMemPtr, message.Length, message.Length, FileAccess.Write);
+            // Write the data.
+            writeStream.Write(message, 0, message.Length);
+
+            // Close the stream.
+            writeStream.Close();
+
+            if (this.offsetCount >= WORD_OFFSET) {
+                // Create another UnmanagedMemoryStream object using a pointer to unmanaged memory.
+                UnmanagedMemoryStream readStream = new UnmanagedMemoryStream(this.readerMemPtr, message.Length, message.Length, FileAccess.Read);
+
+                // Create a byte array to hold data from unmanaged memory.
+                byte[] outMessage = new byte[message.Length];
+
+                // Read from unmanaged memory to the byte array.
+                readStream.Read(outMessage, 0, message.Length);
+
+                // Close the stream.
+                readStream.Close();
+
+                // Display the data to the console.
+                UInt16 readX = (UInt16) outMessage[0];
+                readX = ((ushort) (readX << 8));
+                readX |= (UInt16) outMessage[1];
+
+                UInt16 readY = (UInt16) outMessage[2];
+                readY = ((ushort) (readY << 8));
+                readY |= (UInt16) outMessage[3];
+                label2.Text = "" + readX + ", " + readY;
+                this.readerMemPtr += 32;
+            }
+
+            if (this.offsetCount < WORD_OFFSET) offsetCount++;
+
+            this.generatorMemPtr += 32;
+
+            if (this.generatorMemPtr >= this.beginPtr + (WORD_OFFSET * 4 * 2 * 8)) {
+                this.generatorMemPtr = this.beginPtr;
+            }
+            
+            if (this.readerMemPtr >= this.beginPtr + (WORD_OFFSET * 4 * 2 * 8)) {
+                this.readerMemPtr = this.beginPtr;
+            }
+        }
+    }
+}
