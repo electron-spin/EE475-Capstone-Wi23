88e3832db1237e366595e65e9250df7986262703
Noah Wager <noahwager@gmail.com>
2023-01-30 18:39:37 -0800

    read file at 30hz

--- examples/ipc-fs/reader.cpp
+++ examples/ipc-fs/reader.cpp
@@ -3,12 +3,13 @@
 #include <string>
 #include <unistd.h>
 #include <chrono>
+#include <time.h>
 
 int main(int argc, char **argv) {
-    while (true) {
-        std::cout << "Start" << std::endl;
+    while (1) {
+    std::cout << "" << std::endl;
         auto start = std::chrono::high_resolution_clock::now();
-        std::fstream file("test.txt");
+        std::fstream file("../../test.txt");
         std::string buf;
         if (file.is_open()) {
             // keep reopening file since it's getting modified
@@ -18,7 +19,11 @@ int main(int argc, char **argv) {
         auto stop = std::chrono::high_resolution_clock::now();
         auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
         std::cout << "Read: " << buf << " in " << duration.count() << " us";
-        sleep(1);
+
+        struct timespec tim, tim2;
+        tim.tv_sec = 0;
+        tim.tv_nsec = 33 * 1000000UL;
+        nanosleep(&tim, &tim2);
     }
     return 0;
 }
